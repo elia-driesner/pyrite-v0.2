b2be9342a12414972b83e2d7bdd2fd179359c0dc
elia_driesner <78803906+eliaDr@users.noreply.github.com>
2023-03-29 17:57:34 +0200

    rect row col counting wokring

--- pyrite/assets/rect_cutter.py
+++ pyrite/assets/rect_cutter.py
@@ -5,6 +5,8 @@ def cut_rect(image):
     rect = image.get_rect()
     size = (rect.w, rect.h)
     surf = pygame.Surface(size)
+    height = 0
+    width = 0
     
     for row in range(0, size[1]):
         row_surf = pygame.Surface((size[0], 1))
@@ -12,14 +14,18 @@ def cut_rect(image):
         row_surf.blit(image, (0, 0), (0, row, size[0], 1))
         row_mask = pygame.mask.from_surface(row_surf)
         row_pixels = row_mask.count()
+        if row_pixels != 0:
+            height += 1
         print(row_pixels)
     
     for col in range(0, size[0]):
         col_surf = pygame.Surface((1, size[1]))
         col_surf.set_colorkey((0,0,0))
-        col_surf.blit(image, (0, 0), (0, col, 1, size[1]))
+        col_surf.blit(image, (0, 0), (col, 0, 1, size[1]))
         col_mask = pygame.mask.from_surface(col_surf)
         col_pixels = col_mask.count()
+        if col_pixels != 0:
+            width += 1
         print(col_pixels)
         
         
