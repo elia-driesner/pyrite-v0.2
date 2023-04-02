645d1688ae05ad0099fc3ea68ee3501db7a62bc5
elia_driesner <78803906+eliaDr@users.noreply.github.com>
2023-04-03 00:08:34 +0200

    chanign skins working

--- pyrite/assets/palette_swap.py
+++ pyrite/assets/palette_swap.py
@@ -7,12 +7,15 @@ class PaletteSwap:
     def swap(self, input_image, colors):
         image = input_image
         image_size = image.get_size()
-        img_mask = pygame.mask.from_surface(image)
+        temp_img = pygame.Surface(image_size)
+        
+        temp_img.fill((0, 0, 0))
+        temp_img.blit(image, (0, 0))
+        
         temp_surf = pygame.Surface(image_size)
-        
-        temp_surf.blit(img_mask.to_surface(setcolor=(colors[1]), unsetcolor=[0, 0, 0]), (0, 0))
-        image.set_colorkey(colors[0])
-        temp_surf.blit(image, (0, 0))
-        
+        temp_surf.fill(colors[1])
+        temp_img.set_colorkey(colors[0])
+        temp_surf.blit(temp_img, (0, 0))
         temp_surf.set_colorkey((0, 0, 0))
+        
         return temp_surf
