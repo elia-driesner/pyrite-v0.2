6d805bbaed735c70199c2a87d6c97944a3532d40
elia_driesner <78803906+eliaDr@users.noreply.github.com>
2023-04-06 13:53:23 +0200

    made camera stop when to much left

--- pyrite/camera.py
+++ pyrite/camera.py
@@ -10,6 +10,11 @@ class Camera:
     def calc_scroll(self):
         self.scroll[0] += int((self.focus.rect.x  - self.scroll[0] - (self.game.window.wn_size[0] / 2)) / self.scroll_smoothing)
         self.scroll[1] += int((self.focus.rect.y - self.scroll[1] - (self.game.window.wn_size[1] / 2)) / self.scroll_smoothing)
+        
+        if self.scroll[0] <= 0:
+            self.scroll[0] = 0
+        if self.scroll[1] <= 0:
+            self.scroll[1] = 0
     
     def set_focus(self, entity):
         self.focus = entity
